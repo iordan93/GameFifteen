0fa4fb5456054174b101eb03c649e0b11f8b6561
Cherokee <simeonov23@yahoo.com>
2013-05-13 21:52:26 +0300

    Changes in Main code
    Rename project and main code cs file to GameFifteen.
    Extracted class Pozicia to new file and renamed it to Position + change
    references in class GameFifteen. Added documentation to Position class
    and cjhecks for incorect values for row & column.

---
+++ Code_Refactored/Position.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace GameFifteen
+{
+    public class Position
+    {
+        private int row;
+        private int column;
+
+        /// <summary>
+        /// Initializes an instance of <see cref="Position"/> class.
+        /// </summary>
+        /// <param name="row">Row number.</param>
+        /// <param name="column">Column number.</param>
+        public Position(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        /// <summary>
+        /// Gets or sets the column.
+        /// </summary>
+        /// <value>The column.</value>
+        public int Column
+        {
+            get
+            {
+                return this.column;
+            }
+            set
+            {
+                if (value < 0 )
+                {
+                    throw new ArgumentOutOfRangeException("It is not allowed column to be negative number!");
+                }
+
+                this.column = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the row.
+        /// </summary>
+        /// <value>The row.</value>
+        public int Row
+        {
+            get
+            {
+                return this.row;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("It is not allowed row to be negative number!");
+                }
+
+                this.row = value;
+            }
+        }
+    }
+}
---
+++ Code_Tests/GameFifteen.Test/PositionTest.cs
@@ -0,0 +1,62 @@
+﻿using GameFifteen;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GameFifteen.Test
+{
+    
+    
+    /// <summary>
+    ///This is a test class for PositionTest and is intended
+    ///to contain all PositionTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class PositionTest
+    {
+        /// <summary>
+        ///A test for Position Constructor
+        ///</summary>
+        [TestMethod()]
+        public void PositionConstructorTest()
+        {
+            int row = 0; // TODO: Initialize to an appropriate value
+            int column = 0; // TODO: Initialize to an appropriate value
+            Position target = new Position(row, column);
+            Assert.Inconclusive("TODO: Implement code to verify target");
+        }
+
+        /// <summary>
+        ///A test for Column
+        ///</summary>
+        [TestMethod()]
+        public void ColumnTest()
+        {
+            int row = 0; // TODO: Initialize to an appropriate value
+            int column = 0; // TODO: Initialize to an appropriate value
+            Position target = new Position(row, column); // TODO: Initialize to an appropriate value
+            int expected = 0; // TODO: Initialize to an appropriate value
+            int actual;
+            target.Column = expected;
+            actual = target.Column;
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+
+        /// <summary>
+        ///A test for Row
+        ///</summary>
+        [TestMethod()]
+        public void RowTest()
+        {
+            int row = 0; // TODO: Initialize to an appropriate value
+            int column = 0; // TODO: Initialize to an appropriate value
+            Position target = new Position(row, column); // TODO: Initialize to an appropriate value
+            int expected = 0; // TODO: Initialize to an appropriate value
+            int actual;
+            target.Row = expected;
+            actual = target.Row;
+            Assert.AreEqual(expected, actual);
+            Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
