8ce8c2e257574c3c1aa0a8169f89949013c92744
Cherokee <simeonov23@yahoo.com>
2013-05-13 22:07:07 +0300

    Shell for new Matrix class created
    TODO: move all methods from GameFifteen here and modify code.

--- Code_Refactored/GameFifteen.cs
+++ Code_Refactored/GameFifteen.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace GameFifteen
+﻿namespace GameFifteen
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     public class GameFifteen
     {
         private static void Generatematricata(string[,] matrica)
---
+++ Code_Refactored/Matrix.cs
@@ -0,0 +1,12 @@
+﻿namespace GameFifteen
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Matrix
+    {
+        //TODO: Extract all methods from GameFifteen class related to that class
+    }
+}
