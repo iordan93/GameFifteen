0f8e82eb83b4ef4e14884b6ca3446e3274904ed8
Antonia Stoeva <Antonia.Stoeva@gmail.com>
2013-05-15 16:34:37 +0300

    Add Test for GameFifteen.cs
    Add Test for GameFifteen - IsSolvable method testing.

---
+++ Code_Tests/GameFifteenTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameFifteen;
+
+namespace GameFifteen.Test
+{
+    [TestClass]
+    public class GameFifteenTest
+    {
+        //[TestMethod]
+        //public void IsSolvable_WithUnsolvableField()
+        //{
+        //    string[,] unsolvableGameField = { { "1", "2", "3", "4" },
+        //                                        { "5", "6", "7", "8" },
+        //                                        { "9", "10", "11", "12" },
+        //                                        { "13", "15", "14", " " } };
+
+        //    Assert.IsFalse(GameFifteen.IsSolvable(unsolvableGameField));
+        //}
+    }
+}
