71a851074135e589f34a8971f95412aa3e6ee405
Antonia Stoeva <Antonia.Stoeva@gmail.com>
2013-05-15 11:20:28 +0300

    Merge Branches
    Merge Branches to update from original repository.

--- Code_Refactored/GameFifteen.cs
+++ Code_Refactored/GameFifteen.cs
@@ -7,52 +7,132 @@
 
     public class GameFifteen
     {
-        private static void Generatematricata(string[,] matrica)
+        // Like Settings...
+        private const int GAME_FIELD_ROWS = 4;
+        private const int GAME_FIELD_COLUMNS = 4;
+
+        public static string[,] GenerateGameField()
         {
-            Random rnd = new Random();
-            List<int> used = new List<int>();
-            bool isFilled = false;
+            string[,] gameField = new string[GAME_FIELD_ROWS, GAME_FIELD_COLUMNS];
 
-            matrica[rnd.Next(4), rnd.Next(4)] = " ";
-
-            for (int i = 0; i < 4; i++)
+            do
             {
-                for (int j = 0; j < 4; j++)
-                {
+                Position blankPosition = findPrazno(gameField);
 
-                    isFilled = false;
+                if (blankPosition != null)
+                {
+                    gameField[blankPosition.Row, blankPosition.Column] = "0";
+                }
+
+                FillOutGameField(gameField);
+            }
+            while (!IsSolvable(gameField));
+
+            return gameField;
+        }
+
+        private static void FillOutGameField(string[,] gameField)
+        {
+            Random rand = new Random();
+            List<int> usedNumbers = new List<int>();
+            bool isPositionFilled = false;
+
+            gameField[rand.Next(GAME_FIELD_ROWS), rand.Next(GAME_FIELD_COLUMNS)] = " ";
+
+            for (int i = 0; i < gameField.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameField.GetLength(1); j++)
+                {
+                    isPositionFilled = false;
+
                     do
                     {
-                        int number = rnd.Next(1, 16);
+                        int number = rand.Next(1, 16);
 
-                        if (matrica[i, j] == " ")
+                        if (gameField[i, j] == " ")
                         {
-                            isFilled = true;
+                            isPositionFilled = true;
                         }
-                        if (matrica[i, j] == null)
+                        else
                         {
-                            if (!used.Contains(number))
+                            if (!usedNumbers.Contains(number))
                             {
-                                matrica[i, j] = number.ToString();
-                                isFilled = true;
-                                used.Add(number);
+                                gameField[i, j] = number.ToString();
+                                isPositionFilled = true;
+                                usedNumbers.Add(number);
                             }
                         }
-
                     }
-                    while (isFilled == false);
+                    while (isPositionFilled == false);
                 }
             }
+        }
+
+        // TODO: Place a comment about this method with reference!
+        // http://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
+        private static bool IsSolvable(string[,] gameField)
+        {
+            int[] numbersInOneRow = new int[gameField.Length];
+            int index = 0;
+            Position blankPosition = null;
+
+            for (int i = 0; i < gameField.GetLength(0); i++)
+            {
+                for (int j = 0; j < gameField.GetLength(1); j++)
+                {
+                    if (gameField[i, j] == " ")
+                    {
+                        numbersInOneRow[index] = 0;
+                        blankPosition = new Position(i, j);
+                    }
+                    else
+                    {
+                        numbersInOneRow[index] = int.Parse(gameField[i, j]);
+                    }
+
+                    index++;
+                }
+            }
+
+            int numberOfInversions = 0;
+
+            for (int i = 0; i < numbersInOneRow.Length; i++)
+            {
+                int number = numbersInOneRow[i];
+
+                if (number > 1)
+                {
+                    for (int n = i + 1; n < numbersInOneRow.Length; n++)
+                    {
+                        if (numbersInOneRow[n] < number)
+                        {
+                            numberOfInversions++;
+                        }
+                    }
+                }
+            }
+
+            bool isFieldWidthEven = gameField.GetLength(1) % 2 == 0;
+            bool isNumberOfInversionsEven = numberOfInversions % 2 == 0;
+            bool isBlankOnOddRowFromBottom =
+                (gameField.GetLength(0) - 1 - blankPosition.Row) % 2 == 0;
+
+            bool isSolvable = ((!isFieldWidthEven) && isNumberOfInversionsEven) ||
+                (isFieldWidthEven && (isBlankOnOddRowFromBottom == isNumberOfInversionsEven));
+
+            return isSolvable;
         }
 
         private static void Drawmatrica(string[,] matrica)
         {
             Console.WriteLine("  - - - - - -");
 
+            // TODO: Fix the "magic" number!
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
+                    // TODO: Can this be done without so many checks every cycle/loop?
                     if (j == 0)
                     {
                         Console.Write("| {0,2} ", matrica[i, j]);
@@ -74,14 +154,14 @@
         private static bool proverka(string[,] matrica)
         {
             int counter = 1;
-
+            // TODO: Fix the "magic" number!
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
                     if (matrica[i, j] != counter.ToString())
                     {
-                        if (counter == 15 && matrica[i, j] == " ")
+                        if (counter == 16 && matrica[i, j] == " ")
                         {
                             return true;
                         }
@@ -102,7 +182,8 @@
 
         private static Position findPrazno(string[,] matrica)
         {
-            Position result = new Position(-1, -1);
+            Position result = null;
+            // TODO: Fix the "magic number"!
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
@@ -119,6 +200,7 @@
 
         private static void ChangeAndDraw(string[,] matrica, int rowToChange, int columnToChange, int row, int column, string input)
         {
+            // TODO: Spaghetti :P
             matrica[rowToChange, columnToChange] = input;
             matrica[row, column] = " ";
             Drawmatrica(matrica);
@@ -126,6 +208,7 @@
 
         private static Position FindCurrentElement(string[,] matrica, string input)
         {
+            // TODO: Fix "magic" number!
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
@@ -140,45 +223,49 @@
             return null;
         }
 
-        static void Main(string[] argumentite)
+        static void Main(string[] args)
         {
-            string[,] matrica = new string[4, 4];
+            // TODO: Scenario:
+            // Create scoreboard. Print welcome msg. (Create game field. Check if solvable.) Wait for user command until "exit" (switch). "restart" -> go back to print msg?
+            // "top" -> show scoreboard if any records. "exit" -> print bye, exit loop. DEFAULT: Is INPUT number? Is number in range? -> Illegal number! Is number able to be moved?
+            // -> Illegal move! Illegal command! | If game finished (puzzle solved) -> scoreboard.method...
+            string[,] gameField = GameFifteen.GenerateGameField();
             int moves = 0;
-
+            // TODO: Extract to method the welcome message? And with const?
             Console.WriteLine("Welcome to the game \"15\". Please try to arrange the numbers " +
                 "sequentially .\nUse 'top' to view the top scoreboard, 'restart' to start a new " +
                 "game and 'exit' \nto quit the game.\n\n\n");
 
-            Generatematricata(matrica);
-            Drawmatrica(matrica);
+            Drawmatrica(gameField);
 
-            while (!proverka(matrica))
+            while (!proverka(gameField))
             {
                 Console.Write("Enter a number to move : ");
                 string вход = Console.ReadLine();
                 bool празно = false;
 
 
-                int rowEmptySpace = findPrazno(matrica).Row;
-                int columnEmptySpace = findPrazno(matrica).Column;
+                int rowEmptySpace = findPrazno(gameField).Row;
+                int columnEmptySpace = findPrazno(gameField).Column;
 
                 if (вход == "exit")
                 {
+                    // TODO: Not implemented!
                     Console.WriteLine("Good bye !");
                 }
 
                 if (вход == "restart")
                 {
                     Console.WriteLine("Here is your new matrica, have a good play : \n\n\n");
-                    matrica = new string[4, 4];
-                    Generatematricata(matrica);
-                    Drawmatrica(matrica);
+                    gameField = GameFifteen.GenerateGameField();
+                    Drawmatrica(gameField);
                     moves = 0;
                     continue;
                 }
 
                 if (вход == "top")
                 {
+                    // TODO: Draw if records are presented. Extract method. In class.
                     for (int i = 0; i < 5; i++)
                     {
                         Console.WriteLine("Name : {0} , moves : {1} ", TopScores.igrachi[i], TopScores.hodove[i]);
@@ -187,18 +274,18 @@
 
                 }
 
-                if (FindCurrentElement(matrica, вход) == null)
+                if (FindCurrentElement(gameField, вход) == null)
                 {
                     continue;
                 }
 
-                int rowCurrentElement = FindCurrentElement(matrica, вход).Row;
-                int columnCurrentElement = FindCurrentElement(matrica, вход).Column;
+                int rowCurrentElement = FindCurrentElement(gameField, вход).Row;
+                int columnCurrentElement = FindCurrentElement(gameField, вход).Column;
 
 
 
 
-
+                // TODO: Magic, magic, magic... Method?
                 for (int i = 0; i < 4; i++)
                 {
                     if (i == 0)
@@ -209,9 +296,9 @@
                         }
                         else
                         {
-                            if (matrica[rowCurrentElement - 1, columnCurrentElement] == " ")
+                            if (gameField[rowCurrentElement - 1, columnCurrentElement] == " ")
                             {
-                                ChangeAndDraw(matrica, rowCurrentElement - 1, columnCurrentElement, rowCurrentElement, columnCurrentElement, вход);
+                                ChangeAndDraw(gameField, rowCurrentElement - 1, columnCurrentElement, rowCurrentElement, columnCurrentElement, вход);
                                 празно = true;
                                 moves++;
                             }
@@ -225,9 +312,9 @@
                         }
                         else
                         {
-                            if (matrica[rowCurrentElement + 1, columnCurrentElement] == " ")
+                            if (gameField[rowCurrentElement + 1, columnCurrentElement] == " ")
                             {
-                                ChangeAndDraw(matrica, rowCurrentElement + 1, columnCurrentElement, rowCurrentElement, columnCurrentElement, вход);
+                                ChangeAndDraw(gameField, rowCurrentElement + 1, columnCurrentElement, rowCurrentElement, columnCurrentElement, вход);
                                 празно = true;
                                 moves++;
                             }
@@ -241,9 +328,9 @@
                         }
                         else
                         {
-                            if (matrica[rowCurrentElement, columnCurrentElement - 1] == " ")
+                            if (gameField[rowCurrentElement, columnCurrentElement - 1] == " ")
                             {
-                                ChangeAndDraw(matrica, rowCurrentElement, columnCurrentElement - 1, rowCurrentElement, columnCurrentElement, вход);
+                                ChangeAndDraw(gameField, rowCurrentElement, columnCurrentElement - 1, rowCurrentElement, columnCurrentElement, вход);
                                 празно = true;
                                 moves++;
                             }
@@ -264,10 +351,10 @@
                         else
                         {
 
-                            if (matrica[rowCurrentElement, columnCurrentElement + 1] == " ")
+                            if (gameField[rowCurrentElement, columnCurrentElement + 1] == " ")
                             {
 
-                                ChangeAndDraw(matrica, rowCurrentElement, columnCurrentElement + 1, rowCurrentElement, columnCurrentElement, вход);
+                                ChangeAndDraw(gameField, rowCurrentElement, columnCurrentElement + 1, rowCurrentElement, columnCurrentElement, вход);
                                 празно = true;
                                 moves++;
 
@@ -290,7 +377,7 @@
 
 
             Console.WriteLine("Your result is {0} moves !", moves);
-
+            // TODO: Extract method. In class. Fix bug! (moves = 0)
             for (int i = 0; i < 5; i++)
             {
                 if (TopScores.hodove[i] > moves)
@@ -304,7 +391,7 @@
                 }
             }
 
-
+            // TODO: A new game doesn't start automatically!
         }
     }
 }
