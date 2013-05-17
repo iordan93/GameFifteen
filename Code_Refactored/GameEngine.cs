94e314eb8c76fc33e1bf5f7613a4721bca89376e
Yordan Darakchiev <iordan93@gmail.com>
2013-05-17 11:53:52 +0300

    Bugfix

---
+++ Code_Refactored/GameEngine.cs
@@ -0,0 +1,24 @@
+﻿namespace GameFifteen
+{
+    using System;
+
+    public class GameEngine
+    {
+        /// <summary>
+        /// Reads the user input from the console.
+        /// </summary>
+        /// <returns>Returns the user input as string.</returns>
+        public static string ReadInput()
+        {
+            string input = Console.ReadLine();
+            return input;
+        }
+
+        public static void DisplayWelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the game \"15\". Please try to arrange the numbers " +
+                    "sequentially .\nUse 'top' to view the top scoreboard, 'restart' to start a new " +
+                    "game and 'exit' \nto quit the game.");
+        }
+    }
+}
